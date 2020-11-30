cb2521584a38d4a41acc0d49e6f565e226b8c62c
Ivanina Ivanova <ivanina.at.ivanova@gmail.com>
2020-11-30 21:14:30 +0200

    Uploading project

---
+++ app/src/main/java/com/example/sqliteexample/ViewEmployeeActivity.java
@@ -0,0 +1,33 @@
+package com.example.sqliteexample;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewEmployeeActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_employee);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        DatabaseHelperClass databaseHelperClass = new DatabaseHelperClass(this);
+        List<EmployeeModelClass> employeeModelClasses = databaseHelperClass.getEmployeeList();
+
+        if (employeeModelClasses.size() > 0){
+            EmployeeAdapterClass employeadapterclass = new EmployeeAdapterClass(employeeModelClasses,ViewEmployeeActivity.this);
+            recyclerView.setAdapter(employeadapterclass);
+        }else {
+            Toast.makeText(this, "There is no employee in the database", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
